6bfe15a536c3eeca7157b9e7133eb05ce42c4690
maksymalist <maksymvovkk@gmail.com>
2023-07-17 00:15:44 -0400

    added led class

--- src/lcd_sequence.py
+++ src/lcd_sequence.py
@@ -1,7 +1,22 @@
 import RPi.GPIO as GPIO
+from utils.led import LED
+from utils.button import Button
+import time
+
+def callback():
+    led = LED(38)
+    led.on()
+    time.sleep(1)
+    led.off()
+    
+    
 
 def idle_mode(lcd, led_green):
     lcd.clear()
     lcd.display("insert junk")
     led_green.on()
+    
+    button = Button(22)
+    print("button pressed")
+    button.press_event(callback)
     
---
+++ utils/button.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+
+class Button:
+    def __init__(self, pin) -> None:
+        self.pin = pin
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+    def press_event(self, callback):
+        GPIO.add_event_detect(self.pin, GPIO.RISING,callback=callback)
