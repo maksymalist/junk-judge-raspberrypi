dd8d642f522b1bd2cacb42cd5ff7818b374461a6
maksymalist <maksymvovkk@gmail.com>
2023-07-20 14:25:26 -0400

    testing progress bar

--- utils/lcd_display.py
+++ utils/lcd_display.py
@@ -89,54 +89,48 @@ class LcdModule:
     charge = 0
     charge_delta = 5
     bar_repr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    while True:
-      # Remember that your sentences can only be 16 characters long!
-      self.screen.lcd_display_string("Battery charge:", 1)
+    # Remember that your sentences can only be 16 characters long!
+    self.screen.lcd_display_string("Battery charge:", 1)
 
-      # Render charge bar:
-      bar_string = ""
-      for i in range(10):
-          if i == 0:
-              # Left character
-              if bar_repr[i] == 0:
-                  # Left empty character
-                  bar_string = bar_string + "{0x01}"
-              else:
-                  # Left full character 
-                  bar_string = bar_string + "{0x00}"
-          elif i == 9:
-              # Right character
-              if bar_repr[i] == 0:
-                  # Right empty character
-                  bar_string = bar_string + "{0x05}"
-              else:
-                  # Right full character
-                  bar_string = bar_string + "{0x04}"
-          else:
-              # Central character
-              if bar_repr[i] == 0:
-                  # Central empty character
-                  bar_string = bar_string + "{0x03}"
-              else:
-                  # Central full character
-                  bar_string = bar_string + "{0x02}"
+    # Render charge bar:
+    bar_string = ""
+    for i in range(10):
+        if i == 0:
+            # Left character
+            if bar_repr[i] == 0:
+                # Left empty character
+                bar_string = bar_string + "{0x01}"
+            else:
+                # Left full character 
+                bar_string = bar_string + "{0x00}"
+        elif i == 9:
+            # Right character
+            if bar_repr[i] == 0:
+                # Right empty character
+                bar_string = bar_string + "{0x05}"
+            else:
+                # Right full character
+                bar_string = bar_string + "{0x04}"
+        else:
+            # Central character
+            if bar_repr[i] == 0:
+                # Central empty character
+                bar_string = bar_string + "{0x03}"
+            else:
+                # Central full character
+                bar_string = bar_string + "{0x02}"
 
-      # Print the string to display:
-      self.screen.lcd_display_extended_string(bar_string + " {0}% ".format(charge), 2)
+    # Print the string to display:
+    self.screen.lcd_display_extended_string(bar_string + " {0}% ".format(charge), 2)
 
-      # Update the charge and recalculate bar_repr
-      charge += charge_delta
-      if (charge >= MAX_CHARGE) or (charge <= MIN_CHARGE):
-          charge_delta = -1 * charge_delta
+    # Update the charge and recalculate bar_repr
+    charge += charge_delta
       
-      for i in range(10):
-          if charge >= ((i + 1) * 10):
-              bar_repr[i] = 1
-          else:
-              bar_repr[i] = 0            
-
-      # Wait for some time
-      time.sleep(1) 
+    for i in range(10):
+        if charge >= ((i + 1) * 10):
+            bar_repr[i] = 1
+        else:
+            bar_repr[i] = 0            
 
     
   def clear(self):
