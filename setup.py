a3322738018d3fd55bd071f70406e5ef1f98b62a
maksymalist <maksymvovkk@gmail.com>
2023-07-20 13:29:41 -0400

    added trapdoor

--- setup.py
+++ setup.py
@@ -30,9 +30,7 @@ if __name__ == "__main__":
     
     trapdoor = Button(22)
     
-    
-    machine = JunkJudge(lcd_module, stepper_motor_module, camera_module, led_red, led_green)
-    
+    machine = JunkJudge(lcd_module, stepper_motor_module, camera_module, led_red, led_green, trapdoor)
     machine.on_init()
     
     while True:
