105272f1b933db3d5d5fe6eaf37a2001ad40a740
maksymalist <maksymvovkk@gmail.com>
2023-07-21 12:01:26 -0400

    added more data to output

--- utils/model.py
+++ utils/model.py
@@ -4,10 +4,11 @@ import json
 client = Client("https://maksymalist-junk-judge.hf.space/", serialize=True, hf_token="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")
 
 def predict_type(input_path):
-    # Send the file data directly without serialization
-    with open(input_path, 'rb') as file:
-        result = client.predict(
-            file,               # File data to be sent
-            api_name="/predict"
-        )
-    return result
+
+    result = client.predict(
+		input_path,	# str (filepath or URL to image) in 'image' Image component
+		api_name="/predict"
+    )
+
+
+    return "https://maksymalist-junk-judge.hf.space/"+result
