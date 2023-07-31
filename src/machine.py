5014d82dbec0dd7df53987f9b87c5ac5356da0a5
maksymalist <maksymvovkk@gmail.com>
2023-07-31 08:15:16 -0400

    fixed open error *hopefully*

--- src/machine.py
+++ src/machine.py
@@ -130,8 +130,6 @@ class JunkJudge:
         pass
     
     def toggle_open(self, channel):
-        print("button pressed")
-        print(self.is_open == True)
         
         if self.state != State.IDLE:
             return 
