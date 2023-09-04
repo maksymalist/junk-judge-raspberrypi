69ce7b9d23aa90bf24654691f77b6b4a26275528
maksymalist <maksymvovkk@gmail.com>
2023-09-04 15:39:44 -0400

    added translations

--- utils/languages.py
+++ utils/languages.py
@@ -6,11 +6,11 @@ class Language(Enum):
     FR = 'FR'
 
 class LanguageDictionary:
-    def __init__(self, *dictionaries):
-        self.dictionaries = {lang: dictionary for lang, dictionary in dictionaries}
+    def __init__(self, **kwargs):
+        self.dictionaries = kwargs
 
     def __getitem__(self, key):
         return self.dictionaries[key]
 
-language_dict = LanguageDictionary(ENGLISH, FRENCH)
+language_dict = LanguageDictionary(EN=ENGLISH, FR=FRENCH)
 
