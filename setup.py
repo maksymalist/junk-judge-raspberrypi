f9c28d47651e2013091c6611e396d30fbb67d966
maksymalist <maksymvovkk@gmail.com>
2023-09-04 14:31:43 -0400

    test commit

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         
     # except Exception as e:
     #     print("failed to do something", e)
-    #     machine.failure_sequence()
+    #     machine.failure_sequence() 
 
 
 
