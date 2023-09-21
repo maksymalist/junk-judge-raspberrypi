25dfa493b48f29876253d0167ba97c3d1c54e3be
maksymalist <maksymvovkk@gmail.com>
2023-09-21 16:28:28 -0400

    fixing conflict

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ if __name__ == "__main__":
 
         
         camera = PiCamera()
-        camera = None
         screen = drivers.Lcd()
         
         camera_module = CameraModule(camera, (500, 500), 50)
