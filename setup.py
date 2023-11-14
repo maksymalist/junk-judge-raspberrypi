57274c5aae6acab573721ade0dc517afd64aa288
maksymalist <maksymvovkk@gmail.com>
2023-11-14 17:24:46 -0500

    debugging I/O stuff

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         print("camera and lcd initialized")
         camera_module = CameraModule(camera, (500, 500), 50)
         lcd_module = LcdModule(screen)  
-        conveyor_module_1 = None # BCM 5, 6, 13 respectively
+        conveyor_module_1 = NMotor(29, 31, 33) # BCM 5, 6, 13 respectively
         conveyor_module_2 = NMotor(35, 37, 36) # BCM 19, 26, 16 respectively
         conveyor_module_2.enable()
         conveyor_module_2.rotate_ccw(2000)
